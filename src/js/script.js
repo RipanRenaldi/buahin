b67b06c47902fa50e6bb32659e88dd0734246990
destbreak <rey.hadiansyah7th@gmail.com>
2022-12-09 13:05:22 +0700

    Delete & edit some codes

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,23 @@
 import "../assets/style/style.css";
-import pineapple from "../assets/images/pineapple.jpg";
-import ripanPhoto from "../assets/images/profile/Ripan.jpeg";
-import alfiPhoto from "../assets/images/profile/Alfi.jpg";
-import sitiPhoto from "../assets/images/profile/Dina.jpg";
-import reyPhoto from "../assets/images/profile/Rey.jpg";
-// import selfPhoto from "../assets/images/nophoto.jpg";
-
 import './component/overview-section.js';
 import './component/faq-section.js';
 import './component/profile-section.js';
 import { main } from "./view/main.js";
 
+import pineapple from "../assets/images/pineapple.jpg";
+import profile1 from "../assets/images/profile/Ripan.jpeg";
+import profile2 from "../assets/images/profile/Rey.jpg";
+import profile3 from "../assets/images/profile/Alfi.jpg";
+import profile4 from "../assets/images/profile/Dina.jpg";
+// import selfPhoto from "../assets/images/nophoto.jpg";
+
 window.addEventListener("load", () => {
-  const fruit = document.getElementById("test");
-  fruit.src = pineapple;
-  const svg = document.getElementById("svg");
-  const ripan = document.getElementById("user-ripan");
-  const reynaldi = document.getElementById("user-reynaldi");
-  const alfi = document.getElementById("user-alfi");
-  const siti = document.getElementById("user-siti");
-  ripan.src = ripanPhoto;
-  reynaldi.src = reyPhoto;
-  alfi.src = alfiPhoto;
-  siti.src = sitiPhoto;
-  main();
+  // const svg = document.getElementById("svg");
+  document.getElementById("test").src = pineapple;
+  document.getElementById("user-ripan").src = profile1;
+  document.getElementById("user-reynaldi").src = profile2;
+  document.getElementById("user-alfi").src = profile3;
+  document.getElementById("user-siti").src = profile4;
 });
+
+document.addEventListener('DOMContentLoaded', main);
