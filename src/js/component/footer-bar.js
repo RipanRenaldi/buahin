bdca48255241c09bfbd13c5deccdc0883710dd34
Alfi Safira Az Zahrah <90541793+alfisafira10@users.noreply.github.com>
2022-12-10 23:42:02 +0700

    Add media query

--- src/js/component/footer-bar.js
+++ src/js/component/footer-bar.js
@@ -1,4 +1,4 @@
-class FooterBar extends HTMLElement {
+class HeaderBar extends HTMLElement {
     connectedCallback() {
         this.render();
     }
@@ -6,9 +6,40 @@ class FooterBar extends HTMLElement {
     render() {
         this.innerHTML = 
         `
-        <p>&copy; 2022 | <strong>Team C22-172 SIB Batch 3 Dicoding</strong></p>
+        <style>
+        @media screen and (max-width: 550px) {
+            nav ul li {
+                font-size: 10px;
+                margin-right: 3px;
+            }
+            .logo {
+                font-size: 11px;
+            }
+        }
+
+        @media screen and (max-width: 880px) {
+            nav ul li {
+                font-size: 12px;
+            }
+            .logo {
+                font-size: 15px;
+            }
+        }
+        </style>
+
+        <nav>
+            <div class="logo">
+                <h1>Buahin</h1>
+            </div>
+            <ul>
+                <li><a href="./index.html#">Home</a></li>
+                <li><a href="./index.html#overview">Overview</a></li>
+                <li><a href="./index.html#our-team">Our Team</a></li>
+                <li><a href="./upload.html">Documentation</a></li>
+            </ul>
+        </nav>
         `;
     }
 }
 
-customElements.define('footer-bar', FooterBar);
+customElements.define('header-bar', HeaderBar);
